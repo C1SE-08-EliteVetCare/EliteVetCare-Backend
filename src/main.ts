742c3634367aefb7df5a618ecebf0e6a976bf768
Quang Quoc <quangquoc1542002@gmail.com>
2023-11-03 12:03:19 +0700

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationConfig } from './config/validation.config';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   ValidationConfig(app);
 
   await app.listen(5000);
-
 }
 
 bootstrap();
